62986dbfc0155f3eaeab09406918519cf091acf8
davidik-gyarab <55786095+davidik-gyarab@users.noreply.github.com>
2020-10-04 18:58:17 +0200

    Add files via upload

---
+++ muj-web/src/main/java/MyServlet.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/timezone")
+public class MyServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse res)
+      throws ServletException, IOException {
+    res.setContentType("text/html;charset=utf-8");
+    PrintWriter out = res.getWriter();
+
+    String get = req.getParameter("zone");
+    Set<String> tzs = ZoneId.getAvailableZoneIds();
+    boolean time = false;
+    for (String str : tzs) {
+      if (str != null && str.equals(get)) {
+        time = true;
+        break;
+      }
+    }
+    String info = "Wrong Timezone";
+    if (time == true) {
+      ZoneId zone = ZoneId.of(get);
+      ZonedDateTime posttz = ZonedDateTime.now(zone);
+	info = 	"Your Timezone is" + posttz;
+    }
+
+    out.println("<html><body>");
+    out.println(info);
+    out.println("<a href=`/`>,back</a>");
+    out.println("</body></html>");
+  }
+}
---
+++ muj-web/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+  <form action="timezone" method="get">
+  timezone: <input type="text" name="zone">
+  <input type="submit">
+  </form>
+</body>
+</html>
